215d2da2ab884cdd0635ad4cbcdcbd61b67714cc
Richard <isit_ric@hotmail.com>
2022-07-13 22:08:57 +0100

    hash password

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,8 +1,9 @@
 const {Router}=require("express");
 const userRouter = Router();
+const {hashPassword}=require("../middleware");
 const {createUser, deleteUser}=require("./controllers");
 
-userRouter.post("/user", createUser);
+userRouter.post("/user", hashPassword, createUser);
 userRouter.delete("/:username", deleteUser);
 
 module.exports = userRouter;
